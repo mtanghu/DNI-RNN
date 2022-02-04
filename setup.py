30959a515853bca5d86d7f06d3ee2b185dc3398c
unknown <hu649@purdue.edu>
2022-02-03 19:38:30 -0500

    Commit library files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='DNI-RNN',
+    version='0.1.0',
+    author='Michael Hu',
+    author_email='prmhu@yahoo.com',
+    url='https://github.com/mtanghu/DNI-RNN',
+    description=(
+        'Decoupled Neural Interfaces using Synthetic Gradients for PyTorch'
+    ),
+    py_modules=['dni-rnn'],
+    install_requires=[
+        'torch>=1.0.0'
+    ]
+)
